eff1f4953eb35926258a3ffae9b512d32d49a55e
KainanGB <kainanytbr@gmail.com>
2023-06-01 10:17:20 -0300

    feat: added eslint new configs, prettier and tailwindCSS color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,16 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      'light-purple': '#7b5cfa',
+      'background-dark1': '#1E2139',
+      'background-dark2': '#141625',
+      'white': '#ffffff',
+      'green': '#33D69F',
+      'purpleish': '#747DB5',
+      'light-gray': '#BABDD5',
+      'warning': '#FF8F00',
+      'error': '#EC5757',
     },
   },
   plugins: [],
